5e84f6768975598c26ce170202f29634c4a55588
MatthewMario64 <92827154+MatthewMario64@users.noreply.github.com>
2022-01-21 13:59:56 -0700

    Create SMBEngine.hpp

---
+++ source/SMB/SMBEngine.hpp
@@ -0,0 +1,16 @@
+
+    /**
+     * Run the decompiled code for the game.
+     * 
+     * See SMB.cpp for implementation.
+     *
+     * @param mode the mode to run. 0 runs initialization routines, 1 runs the logic for frames.
+     */
+
+    /**
+     * Load all constant data that was present in the SMB ROM.
+     * 
+     * See SMBData.cpp for implementation.
+     */
+    void loadConstantData();
+
